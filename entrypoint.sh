2db849804733d6a6e3a1eb4d0400b0f5820f9da3
Tomasz Chorwat <tomasz@chorwat.pl>
2020-09-24 00:25:13 +0200

    updated entrypoint to conform https://github.com/docker-library/official-images#consistency

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-[ ! -f package-lock.json ] || [ -d node_modules ] || { echo '' | npm ci --prefer-offline; } 
-{ set -x; "$@"; set +x; }
+set -e
+# check for ng expected command
+if [ "$1" = 'ng' ] || [ -z "$1" ]; then
+	# npm ci if needed
+	[ ! -f package-lock.json ] || [ -d node_modules ] || { echo '' | npm ci --prefer-offline; } 
+    # execute ng
+	if [ "$1" = 'ng' ]; then
+	    exec "$@"
+	fi
+fi
+# else default to run whatever the user wanted like "bash" or "sh"
+exec "$@"
