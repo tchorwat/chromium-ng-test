64a9b880c00466b46ee18aa19cc670d9e25e3c65
Tomasz Chorwat <tomasz@chorwat.pl>
2020-09-27 16:17:59 +0200

    README - license & support information

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 # check for ng expected command
 if [ "$1" = 'ng' ] || [ -z "$1" ]; then
 	# npm ci if needed
-	[ ! -f package-lock.json ] || [ -d node_modules ] || { echo '' | npm ci --prefer-offline; } 
+	[ ! -f package-lock.json ] || { echo '' | npm ci --prefer-offline; } 
     # execute ng
 	if [ "$1" = 'ng' ]; then
 	    exec "$@"
